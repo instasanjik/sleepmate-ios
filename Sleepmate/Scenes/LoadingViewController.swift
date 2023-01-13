a871d2a6bfc260c5ac91d65bafdeaf5324f75118
instasanjik <koshkarbayev.07@gmail.com>
2023-01-13 19:57:37 +0600

    feat: setup folder architecture

---
+++ Sleepmate/Scenes/LoadingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoadingViewController.swift
+//  sleepmate
+//
+//  Created by Sanzhar Koshkarbayev on 13.01.2023.
+//
+
+import UIKit
+
+class LoadingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
