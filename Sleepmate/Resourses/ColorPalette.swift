481a5db12ca1bf38bbaee7ea654af91fe5c20da1
instasanjik <koshkarbayev.07@gmail.com>
2023-01-17 04:45:11 +0600

    feat (resourses): add color palette

---
+++ Sleepmate/Resourses/ColorPalette.swift
@@ -0,0 +1,18 @@
+//
+//  ColorPalette.swift
+//  Sleepmate
+//
+//  Created by Sanzhar Koshkarbayev on 16.01.2023.
+//
+
+import UIKit
+
+class ColorPalette {
+    static let white        = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+    static let black        = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+    
+    static let darkBlue     = #colorLiteral(red: 0.0431372549, green: 0.0431372549, blue: 0.09803921569, alpha: 1)
+    
+    static let lightGray    = #colorLiteral(red: 0.8509803922, green: 0.8509803922, blue: 0.8509803922, alpha: 1)
+    static let darkGray     = #colorLiteral(red: 0.1725490196, green: 0.1725490196, blue: 0.1725490196, alpha: 1)
+}
