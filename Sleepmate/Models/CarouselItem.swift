c2dab104554883f9bdd265bfb4b0193425fef85d
instasanjik <koshkarbayev.07@gmail.com>
2023-01-17 04:43:54 +0600

    feat (models): add carousel imte class

---
+++ Sleepmate/Models/CarouselItem.swift
@@ -0,0 +1,21 @@
+//
+//  CarouselItem.swift
+//  Sleepmate
+//
+//  Created by Sanzhar Koshkarbayev on 17.01.2023.
+//
+
+import Foundation
+
+class CarouselItem {
+    
+    var imageName: String
+    var bodyText: String
+    
+    init(imageName: String, bodyText: String) {
+        self.imageName = imageName
+        self.bodyText = bodyText
+    }
+    
+    
+}
