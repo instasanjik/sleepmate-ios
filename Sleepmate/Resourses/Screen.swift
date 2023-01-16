e941ebf6aa0e2f04f23ab04248df4be9818142da
instasanjik <koshkarbayev.07@gmail.com>
2023-01-17 04:45:36 +0600

    feat (resourses): add screen class

---
+++ Sleepmate/Resourses/Screen.swift
@@ -0,0 +1,18 @@
+//
+//  Screen.swift
+//  Sleepmate
+//
+//  Created by Sanzhar Koshkarbayev on 17.01.2023.
+//
+
+import UIKit
+
+class Screen {
+    static var height: CGFloat {
+        return UIScreen.main.bounds.size.height
+    }
+
+    static var width: CGFloat {
+        return UIScreen.main.bounds.size.width
+    }
+}
