f1060e5259d3cccdaac306d2b217bdb8746be734
instasanjik <koshkarbayev.07@gmail.com>
2023-01-17 04:48:35 +0600

    feat (supporting): add segue to carousel view controller

--- Sleepmate/SupportingFiles/AppDelegate.swift
+++ Sleepmate/SupportingFiles/AppDelegate.swift
@@ -10,10 +10,15 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = CarouselViewController()
+        window.makeKeyAndVisible()
+        self.window = window
+        
         return true
     }
 
