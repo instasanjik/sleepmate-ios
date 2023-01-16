56f8c8afb38dbedcd9060dc5082dd316c30297ee
instasanjik <koshkarbayev.07@gmail.com>
2023-01-17 04:46:33 +0600

    feat (views): create skviewcontroller for overriding status bar style

---
+++ Sleepmate/CustomControls/Views/SKViewContoller.swift
@@ -0,0 +1,14 @@
+//
+//  SKViewContoller.swift
+//  Sleepmate
+//
+//  Created by Sanzhar Koshkarbayev on 17.01.2023.
+//
+
+import UIKit
+
+class SKViewController: UIViewController {
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+          return .lightContent
+    }
+}
