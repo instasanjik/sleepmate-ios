bdb4b51107bed3cbca0818a03f187dd1df2dec63
instasanjik <koshkarbayev.07@gmail.com>
2023-01-17 04:44:24 +0600

    feat (views): add deafault white button class

---
+++ Sleepmate/CustomControls/Views/DefaultWhiteButton.swift
@@ -0,0 +1,39 @@
+//
+//  WhiteButton.swift
+//  Sleepmate
+//
+//  Created by Sanzhar Koshkarbayev on 16.01.2023.
+//
+
+import UIKit
+
+class DefaultWhiteButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        layer.cornerRadius = 14
+        backgroundColor = .white
+        titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .medium)
+        setTitleColor(ColorPalette.black, for: .normal)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupTitle(text: String) {
+        setTitle(text, for: .normal)
+    }
+    
+    func disable() {
+        alpha = 0.7
+        isEnabled = false
+    }
+    
+    func enaable() {
+        alpha = 1
+        isEnabled = true
+    }
+    
+
+}
