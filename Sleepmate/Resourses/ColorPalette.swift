c2e4b7c92400900bbcc1d4b3713796a22f4908ae
instasanjik <koshkarbayev.07@gmail.com>
2023-01-18 02:23:52 +0600

    feat (resourses): add colors to color palette

--- Sleepmate/Resourses/ColorPalette.swift
+++ Sleepmate/Resourses/ColorPalette.swift
@@ -8,11 +8,23 @@
 import UIKit
 
 class ColorPalette {
-    static let white        = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-    static let black        = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
     
     static let darkBlue     = #colorLiteral(red: 0.0431372549, green: 0.0431372549, blue: 0.09803921569, alpha: 1)
     
     static let lightGray    = #colorLiteral(red: 0.8509803922, green: 0.8509803922, blue: 0.8509803922, alpha: 1)
     static let darkGray     = #colorLiteral(red: 0.1725490196, green: 0.1725490196, blue: 0.1725490196, alpha: 1)
+    
+    static let yellow       = #colorLiteral(red: 0.9843137255, green: 0.8745098039, blue: 0.4392156863, alpha: 1)
+    
+    //basic colors
+    static let white: UIColor           = .white
+    static let black: UIColor           = .black
+    
+    // gray shades
+    static let systemGray: UIColor      = .systemGray
+    static let systemGray2: UIColor     = .systemGray2
+    static let systemGray3: UIColor     = .systemGray3
+    static let systemGray4: UIColor     = .systemGray4
+    static let systemGray5: UIColor     = .systemGray5
+    static let systemGray6: UIColor     = .systemGray6
 }
