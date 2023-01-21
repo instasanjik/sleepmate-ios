19300a9bb0f9e9692e23cd257053003be0fac13e
instasanjik <koshkarbayev.07@gmail.com>
2023-01-21 13:41:06 +0600

    feat (scenes): create loading screen

---
+++ Sleepmate/Scenes/LoadingViewController.swift
@@ -0,0 +1,39 @@
+//
+//  LoadingViewController.swift
+//  Sleepmate
+//
+//  Created by Sanzhar Koshkarbayev on 21.01.2023.
+//
+
+import UIKit
+import SnapKit
+import Lottie
+
+class LoadingViewController: UIViewController {
+    lazy var infomarionSearchingAnimation = LottieAnimationView(name: "InformationSearchingAnimation")
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = ColorPalette.darkBlue
+        setupAnimationView()
+    }
+    
+
+}
+
+extension LoadingViewController {
+    
+    func setupAnimationView() {
+        view.addSubview(infomarionSearchingAnimation)
+        infomarionSearchingAnimation.play()
+        infomarionSearchingAnimation.loopMode = .loop
+        infomarionSearchingAnimation.snp.makeConstraints { make in
+            make.center.equalTo(view)
+            make.left.equalTo(view).inset(126)
+            make.height.greaterThanOrEqualTo(86)
+            make.height.lessThanOrEqualTo(200)
+        }
+    }
+    
+    
+}
