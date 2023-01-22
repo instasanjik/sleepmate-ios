914274f8f9515670dc6f8d43610fd89c37bf9c76
instasanjik <koshkarbayev.07@gmail.com>
2023-01-22 21:47:22 +0600

    feat (models): create user model

---
+++ Sleepmate/Models/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  Sleepmate
+//
+//  Created by Sanzhar Koshkarbayev on 21.01.2023.
+//
+
+import Foundation
+
+class User {
+    
+    var name: String = ""
+    var username: String = ""
+    
+    
+    
+}
