a91bbf3ef283dc9a8749b98e9341c77f822b275e
instasanjik <koshkarbayev.07@gmail.com>
2023-01-26 04:58:32 +0600

    feat (models): create daily statistics model

--- Sleepmate/Models/DailyStatistic.swift
+++ Sleepmate/Models/DailyStatistic.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+class DailyStatistic {
+    
+    var date: Date
+    var sleepDuration: Double
+    var deepSleepDuration: Double
+    var wakeUpCount: Int
+    var bedTime: Date
+    var wakeUpTime: Date
+    
+    var sleepEfficiency: Double {
+        return sleepDuration / (wakeUpTime.timeIntervalSince(bedTime) / 3600)
+    }
+    
+    var fellAsleepTime: Date {
+       return wakeUpTime.addingTimeInterval(-sleepDuration)
+    }
+    
+    init(date: Date, sleepDuration: Double, deepSleepDuration: Double, wakeUpCount: Int, bedTime: Date, wakeUpTime: Date) {
+        self.date = date
+        self.sleepDuration = sleepDuration
+        self.deepSleepDuration = deepSleepDuration
+        self.wakeUpCount = wakeUpCount
+        self.bedTime = bedTime
+        self.wakeUpTime = wakeUpTime
+    }
+    
+    
+}
