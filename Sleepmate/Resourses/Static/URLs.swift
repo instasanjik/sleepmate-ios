654b2775d04727420d9ba38ff0105fc839a0734a
instasanjik <koshkarbayev.07@gmail.com>
2023-01-26 04:56:42 +0600

    feat (resourses): create class for urls

--- Sleepmate/Resourses/Static/URLs.swift
+++ Sleepmate/Resourses/Static/URLs.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+class URLs {
+    fileprivate static var SERVER_IP = "https://63cd9b7ffba6420d4d721fa6.mockapi.io/"
+    
+    fileprivate static let API_VERSION = "api/v1/"
+    
+    static let STATISTIC = SERVER_IP + API_VERSION + "statistics"
+}
