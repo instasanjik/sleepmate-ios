050c9777af62554d489d81d06a206a7ccc3352fc
instasanjik <koshkarbayev.07@gmail.com>
2023-01-26 04:57:57 +0600

    feat (custom-controls): add collection view extention

--- Sleepmate/CustomControls/Extensions/UIKit/UICollectionView.swift
+++ Sleepmate/CustomControls/Extensions/UIKit/UICollectionView.swift
@@ -5,4 +5,15 @@
 //  Created by Sanzhar Koshkarbayev on 26.01.2023.
 //
 
-import Foundation
+import UIKit
+
+extension UICollectionView {
+    func scrollToLastItem(at scrollPosition: UICollectionView.ScrollPosition = .centeredHorizontally, animated: Bool = true) {
+        let lastSection = numberOfSections - 1
+        guard lastSection >= 0 else { return }
+        let lastItem = numberOfItems(inSection: lastSection) - 1
+        guard lastItem >= 0 else { return }
+        let lastItemIndexPath = IndexPath(item: lastItem, section: lastSection)
+        scrollToItem(at: lastItemIndexPath, at: scrollPosition, animated: animated)
+    }
+}
