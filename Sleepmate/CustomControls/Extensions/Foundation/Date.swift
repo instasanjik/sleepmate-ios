d3886f30cad3f440b82f55e1b7e06e797edd6b15
instasanjik <koshkarbayev.07@gmail.com>
2023-01-26 04:54:07 +0600

    feat (custom-controls): add date exteions

--- Sleepmate/CustomControls/Extensions/Foundation/Date.swift
+++ Sleepmate/CustomControls/Extensions/Foundation/Date.swift
@@ -8,9 +8,21 @@
 import Foundation
 
 extension Date {
+    
     static var dateTime: String {
         get {
             return Date.now.ISO8601Format()
         }
     }
+    
+    var dayOfWeek: String {
+        get {
+            let weekDayNamesShort = ["Sn", "Mn", "Tu", "Wd", "Th", "Fr", "St"]
+            let dayNumber = Calendar.current.dateComponents([.weekday], from: self).weekday
+            guard let dayNumber = dayNumber else { return "" }
+            return weekDayNamesShort[dayNumber-1]
+        }
+    }
+    
+    
 }
