40706cc7b5e34d866be541f509d996c84c90b108
instasanjik <koshkarbayev.07@gmail.com>
2023-01-30 20:46:41 +0600

    feat (models): add fields to user model

--- Sleepmate/Models/User.swift
+++ Sleepmate/Models/User.swift
@@ -7,11 +7,27 @@
 
 import Foundation
 
+enum Gender {
+    case man
+    case woman
+} // it is not a social network, so, I will not add some special gender types or case as "do not prefer to say"
+
 class User {
     
     var name: String = ""
+    var lastName: String = ""
     var username: String = ""
+    var avatarLink: String?
+    var gender: Gender
     
-    
+    init(name: String, lastName: String, username: String? = nil, avatarLink: String?, gender: Gender) {
+        self.name = name
+        self.lastName = lastName
+        if let username = username {
+            self.username = username
+        }
+        self.avatarLink = avatarLink
+        self.gender = gender
+    }
     
 }
